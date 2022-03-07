a15791e2acaee0dd522f3357c811cb5b9acc357e
Ansel <101030120+Ansel12306@users.noreply.github.com>
2022-03-07 09:12:44 +0800

    add lab2

---
+++ College grade 2/Python 3/Lab_2/chapter 2/exercise 1.py
@@ -0,0 +1,10 @@
+try:
+    a=int(input("Please enter a number:"))
+    if a<100:
+        a = str(a)
+        print(a[0])
+        print(a[1])
+    else:
+        print("no correct")
+except:
+    print("no correct")
