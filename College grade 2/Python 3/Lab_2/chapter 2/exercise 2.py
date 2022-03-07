173142ef8bb9ee28835bd48c160e3209a7ac7f62
Ansel <101030120+Ansel12306@users.noreply.github.com>
2022-03-07 10:10:16 +0800

    Update exercise 2.py

--- College grade 2/Python 3/Lab_2/chapter 2/exercise 2.py
+++ College grade 2/Python 3/Lab_2/chapter 2/exercise 2.py
@@ -1,9 +1,13 @@
 try:
     a=int(input("Please enter the number of seconds:"))
-    day=divmod(a,86400)[0]
-    hours=divmod(a,86400)[1]//3600
-    minutes=(a-86400*day-3600*hours)//60
-    seconds=(a-86400*day-3600*hours-60*minutes)
-    print(a,"seconds correspond to",day,"day,",hours,"hours,",minutes,"minutes,and",seconds,"seconds")
+    if a>=0:
+        day=divmod(a,86400)[0]
+        hours=divmod(a,86400)[1]//3600
+        minutes=(a-86400*day-3600*hours)//60
+        seconds=(a-86400*day-3600*hours-60*minutes)
+        print(a,"seconds correspond to",day,"day,",hours,"hours,",minutes,"minutes,and",seconds,"seconds")
+    else:
+        print("no correct")
 except:
     print("no correct")
+
