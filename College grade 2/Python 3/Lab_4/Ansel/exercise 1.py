6a0f4495f39e28439ca33779b007ebd3abf4208c
Ansel <101030120+Ansel12306@users.noreply.github.com>
2022-03-26 12:24:05 +0800

    Update exercise 1.py

--- College grade 2/Python 3/Lab_4/Ansel/exercise 1.py
+++ College grade 2/Python 3/Lab_4/Ansel/exercise 1.py
@@ -2,12 +2,14 @@ def div(a,b):
     i=0
     while True:
         if i*b >a:
-            return i-1
+            c=i-1
             break
         elif i*b==a:
-            return i
+            c=i
+            break
         else:
             i=i+1
+    return (a-c*b)
 
 a=float(input("Please enter the first number:"))
 b=float(input("Please enter the second number:"))
